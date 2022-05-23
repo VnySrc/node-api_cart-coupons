68cc9af1f3a1d3cf111e049f3ab6e273352acf21
VnySrc <vinyprog.work@gmail.com>
2022-05-22 22:41:56 -0300

    make it better

--- src/database/sequelize-cli/config/config.js
+++ src/database/sequelize-cli/config/config.js
@@ -17,10 +17,9 @@ module.exports = {
     "dialect": "postgres",
   },
   "production": {
-   " url": process.env.DATABASE_URL,
+    "url": process.env.DATABASE_URL,
     "dialect": "postgres",
     "ssl": true, 
     "extra": { "ssl": { "rejectUnauthorized": false }}
   }
 }
-
