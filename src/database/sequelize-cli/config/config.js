01cebfa1e48825e25a4f986b931d42b4e9abb661
VnySrc <vinyprog.work@gmail.com>
2022-05-24 10:43:50 -0300

    Update

--- src/database/sequelize-cli/config/config.js
+++ src/database/sequelize-cli/config/config.js
@@ -17,10 +17,7 @@ module.exports = {
     "dialect": "postgres",
   },
   "production": {
-    "username": process.env.POSTGRES_PRODUCTION_USER,
-    "password": process.env.POSTGRES_PRODUCTION_PWD,
-    "database": process.env.POSTGRES_PRODUCTION_DB,
-    "host": process.env.POSTGRES_PRODUCTION_HOST,
+    "url": "postgres://gvicusqbfkzzcj:cf11abadf5a73ce4512c845b1b573459da75343efe22aaef0ab223fc6ecc08f9@ec2-34-231-177-125.compute-1.amazonaws.com:5432/d61rechmkcjcgk",
     "ssl": { "rejectUnauthorized": false }},
     "dialect": "postgres",
   }
